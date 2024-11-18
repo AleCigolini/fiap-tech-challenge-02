7991c2742b2af1c2103cfaab191083b543991d7e
AleCigolini <cigolini.ale@gmail.com>
2024-11-18 19:08:53 -0300

    Project initialized

---
+++ src/main/java/br/com/fiap/techchallenge01/Techchallenge01Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techchallenge01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Techchallenge01Application.class, args);
+    }
+
+}
