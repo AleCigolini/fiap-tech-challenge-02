4896d8bf7629833f805578c44e04b061064277d4
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2025-01-18 10:59:45 -0300

    Ajustes/Melhorias

--- src/main/java/br/com/fiap/techchallenge01/cliente/adapter/in/controller/ClienteController.java
+++ src/main/java/br/com/fiap/techchallenge01/cliente/adapter/in/controller/ClienteController.java
@@ -1,11 +1,11 @@
 package br.com.fiap.techchallenge01.cliente.adapter.in.controller;
 
 import br.com.fiap.techchallenge01.cliente.adapter.in.controller.api.ClienteApi;
-import br.com.fiap.techchallenge01.cliente.application.service.ClienteService;
+import br.com.fiap.techchallenge01.cliente.application.usecase.ClienteUseCase;
 import br.com.fiap.techchallenge01.cliente.config.mapper.ClienteMapper;
 import br.com.fiap.techchallenge01.cliente.domain.dto.request.ClienteRequestDto;
 import br.com.fiap.techchallenge01.cliente.domain.dto.response.ClienteResponseDto;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,37 +13,37 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/clientes")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ClienteController implements ClienteApi {
-    private ClienteMapper clienteMapper;
-    private ClienteService clienteService;
+    private final ClienteMapper clienteMapper;
+    private final ClienteUseCase clienteUseCase;
 
     @Override
     @GetMapping("/cpf")
     @ResponseStatus(HttpStatus.OK)
     public ClienteResponseDto buscarClientePorCpf(@RequestParam String cpf) {
-        return clienteMapper.toResponse(clienteService.buscarClientePorCpf(cpf));
+        return clienteMapper.toResponse(clienteUseCase.buscarClientePorCpf(cpf));
     }
 
     @Override
     @GetMapping("/email")
     @ResponseStatus(HttpStatus.OK)
     public ClienteResponseDto buscarClientePorEmail(@RequestParam String email) {
-        return clienteMapper.toResponse(clienteService.buscarClientePorEmail(email));
+        return clienteMapper.toResponse(clienteUseCase.buscarClientePorEmail(email));
     }
 
     @Override
     @GetMapping("/id")
     @ResponseStatus(HttpStatus.OK)
     public ClienteResponseDto buscarClientePorId(@RequestParam UUID id) {
-        return  clienteMapper.toResponse(clienteService.buscarClientePorId(id));
+        return  clienteMapper.toResponse(clienteUseCase.buscarClientePorId(id));
     }
 
     @Override
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public ClienteResponseDto cadastrarCliente(@RequestBody ClienteRequestDto clienteRequestDto) {
-        return clienteMapper.toResponse(clienteService.salvarCliente(clienteMapper.requestDtoToDomain(clienteRequestDto)));
+        return clienteMapper.toResponse(clienteUseCase.salvarCliente(clienteMapper.requestDtoToDomain(clienteRequestDto)));
     }
 
 }
--- src/main/java/br/com/fiap/techchallenge01/produto/adapter/in/controller/CategoriaProdutoController.java
+++ src/main/java/br/com/fiap/techchallenge01/produto/adapter/in/controller/CategoriaProdutoController.java
@@ -1,12 +1,12 @@
 package br.com.fiap.techchallenge01.produto.adapter.in.controller;
 
 import br.com.fiap.techchallenge01.produto.adapter.in.controller.api.CategoriaProdutoApi;
-import br.com.fiap.techchallenge01.produto.application.service.CategoriaProdutoService;
+import br.com.fiap.techchallenge01.produto.application.usecase.CategoriaProdutoUseCase;
 import br.com.fiap.techchallenge01.produto.domain.CategoriaProduto;
 import br.com.fiap.techchallenge01.produto.domain.dto.request.CategoriaProdutoRequestDTO;
 import br.com.fiap.techchallenge01.produto.domain.dto.response.CategoriaProdutoResponseDTO;
 import br.com.fiap.techchallenge01.produto.utils.mapper.CategoriaProdutoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +14,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categorias-produto")
+@RequiredArgsConstructor
 public class CategoriaProdutoController implements CategoriaProdutoApi {
 
-    @Autowired
-    private CategoriaProdutoMapper categoriaProdutoMapper;
-
-    @Autowired
-    private CategoriaProdutoService categoriaProdutoService;
+    private final CategoriaProdutoMapper categoriaProdutoMapper;
+    private final CategoriaProdutoUseCase categoriaProdutoUseCase;
 
     @Override
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CategoriaProdutoResponseDTO buscarCategoriaProduto(@PathVariable String id) {
-        return categoriaProdutoMapper.toResponse(categoriaProdutoService.buscarCategoriaProdutoPorId(id));
+        return categoriaProdutoMapper.toResponse(categoriaProdutoUseCase.buscarCategoriaProdutoPorId(id));
     }
 
     @Override
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<CategoriaProdutoResponseDTO> listarCategoriasProduto() {
-        return categoriaProdutoMapper.toCollectionResponse(categoriaProdutoService.buscarCategoriasProduto());
+        return categoriaProdutoMapper.toCollectionResponse(categoriaProdutoUseCase.buscarCategoriasProduto());
     }
 
     @Override
@@ -43,7 +41,7 @@ public class CategoriaProdutoController implements CategoriaProdutoApi {
 
         CategoriaProduto categoriaProduto = categoriaProdutoMapper.toDomain(categoriaProdutoRequestDto);
 
-        return categoriaProdutoMapper.toResponse(categoriaProdutoService.salvarCategoriaProduto(categoriaProduto));
+        return categoriaProdutoMapper.toResponse(categoriaProdutoUseCase.salvarCategoriaProduto(categoriaProduto));
     }
 
     @Override
@@ -51,17 +49,17 @@ public class CategoriaProdutoController implements CategoriaProdutoApi {
     @ResponseStatus(HttpStatus.OK)
     public CategoriaProdutoResponseDTO alterarNomeCategoriaProduto(@PathVariable String id, @RequestBody CategoriaProdutoRequestDTO categoriaProdutoRequestDto) {
 
-        CategoriaProduto categoriaProduto = categoriaProdutoService.buscarCategoriaProdutoPorId(id);
+        CategoriaProduto categoriaProduto = categoriaProdutoUseCase.buscarCategoriaProdutoPorId(id);
         categoriaProduto.setNome(categoriaProdutoRequestDto.getNome());
 
-        return categoriaProdutoMapper.toResponse(categoriaProdutoService.alterarNomeCategoriaProduto(id, categoriaProduto));
+        return categoriaProdutoMapper.toResponse(categoriaProdutoUseCase.alterarNomeCategoriaProduto(id, categoriaProduto));
     }
 
     @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desativarCategoriaProduto(@PathVariable String id) {
-        CategoriaProduto categoriaProduto = categoriaProdutoService.buscarCategoriaProdutoPorId(id);
-        categoriaProdutoService.desativarCategoriaProduto(categoriaProduto);
+        CategoriaProduto categoriaProduto = categoriaProdutoUseCase.buscarCategoriaProdutoPorId(id);
+        categoriaProdutoUseCase.desativarCategoriaProduto(categoriaProduto);
     }
 }
